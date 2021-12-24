7d9ae92208a73e3426198f714f76fe4675049f65
MansoorCM <manzoorcm96@gmail.com>
2021-12-24 13:06:04 +0530

    Created cart and navBar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    NavBarComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
